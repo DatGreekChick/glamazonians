f98bd218331a95f53c7ebc66a8234486e58b4ebb
srackley <shelbyrackleyart@gmail.com>
2017-10-31 11:37:31 -0400

    feat: write backed routes for product with temporary Popular products name

---
+++ server/api/products.js
@@ -0,0 +1,60 @@
+const router = require('express').Router()
+const {Product, Order} = require('../db/models')
+module.exports = router
+
+
+// GET /api/products
+router.get('/', (req, res, next) => {
+  Product.findAll({
+    include: [
+      { model: Order },
+    ],
+  })
+    .then(product => res.json(product))
+    .catch(next);
+});
+
+// GET /api/products/:productId
+router.get('/:productId', (req, res, next) => {
+  Product.findById(req.params.productId)
+  .then(product => res.json(product))
+  .catch(next);
+});
+
+// POST /api/products
+router.post('/', (req, res, next) => {
+  Product.create(req.body)
+  .then(product => res.json(product))
+  .catch(next);
+});
+
+// PUT /api/products
+router.put('/:productId', (req, res, next) => {
+  Product.findById(req.params.productId)
+  .then(product => product.update(req.body))
+  .then(res.end())
+  .catch(next);
+});
+
+// DELETE /api/products/:productId
+router.delete('/:productId', (req, res, next) => {
+  Product.destroy({
+    where:
+    { id: req.params.productId }
+  })
+  .then(() => res.status(204).end())
+  .catch(next);
+});
+
+// GET /api/products
+router.get('/:tag', (req, res, next) => {
+  Product.findSimilarByTag(req.params.tag)
+  .then(products => res.json(products))
+  .catch(next);
+});
+
+router.get('/popular', (req, res, next) => {
+  Order.getPopularProducts({})
+  .then(products => res.json(products))
+  .catch(next);
+});
