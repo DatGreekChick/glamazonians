0324c9fc27ef954e9222ca3cad100d96145bf8be
Eleni A <eleni.arvanitis@me.com>
2017-10-31 17:59:37 -0400

    updated secrets (shouldn't pop up here)

---
+++ secrets.js
@@ -0,0 +1,7 @@
+process.env.GOOGLE_CLIENT_ID = '916615863280-ri9ecd1d2pfetfe5ibte38le90hfg2gv.apps.googleusercontent.com';
+process.env.GOOGLE_CLIENT_SECRET = 'kz73cZ2xcCc4Xi58P4_3NpQ1';
+process.env.GOOGLE_CALLBACK = '/auth/google/verify';
+
+process.env.FACEBOOK_APP_ID = '1672586962764610';
+process.env.FACEBOOK_APP_SECRET = '941b686dcbc0ebb71bc94b86e947e03c';
+process.env.FACEBOOK_CALLBACK = '/auth/facebook/verify';
