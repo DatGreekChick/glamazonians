b5e9edb5d2ddcd27342243e0bacf541be8bbf9b6
KeriPMiller <miller.keripatricia@gmail.com>
2017-11-02 18:29:56 -0400

    adding skeleton

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {getSingleProduct} from '../store/products';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux'
+import {withRouter} from 'react-router';
 
-const SingleProduct = () => {
-  return (
-    <div>
-      SingleProduct
-    </div>
-  );
+class SingleProduct extends Component {
+  constructor() {
+    super()
+  }
+
+  // componentWillMount() {
+  //   const productId = this.props.match.params.productId;
+  //   this.props.getProductDetail(productId);
+  // }
+
+  render() {
+    return (
+      <div>
+        <h2>SingleProduct</h2>
+      </div>
+    );
+  }
 };
 
-export default SingleProduct;
+const mapStateToProps = ({products}) => ({products})
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     getProductDetail: (product) => {
+//       dispatch(getSingleProduct(product));
+//     }
+//   }
+// }
+
+export default connect(mapStateToProps)(SingleProduct);
