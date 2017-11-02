a02b8d3461c32e644abff8dc6ec8dda6b1db1860
KeriPMiller <miller.keripatricia@gmail.com>
2017-11-01 20:56:17 -0400

    added specs to test product getters and setters for price attr

--- server/db/models/product.spec.js
+++ server/db/models/product.spec.js
@@ -8,7 +8,7 @@ describe('Product model)', () => {
   });
 
   describe('Product model attributes', () => {
-    let costume;
+    let costume, costume2;
     beforeEach(() => {
       return Product.create({
         name: 'sexy cat',
@@ -20,9 +20,21 @@ describe('Product model)', () => {
       }).then(product => (costume = product));
     });
 
+    beforeEach(() => {
+      return Product.create({
+        name: 'Sexy Cage',
+        image: 'http://placecage.com/400/400',
+        price: 50.00,
+        quantityAvailable: 1,
+        description: 'Not your typical sexy Cage costume',
+        tags: ['cage', 'meow', 'sexy']
+      }).then(product => (costume2 = product));
+    });
+
     it('returns the quantity available', () => {
       expect(costume.quantityAvailable).to.equal(15);
     });
+
 
     it('returns accurate tag', () => {
       expect(costume.tags[0]).to.equal('cat');
@@ -30,6 +42,19 @@ describe('Product model)', () => {
 
     it('returns number of tags', () => {
       expect(costume.tags.length).to.equal(3);
+    });
+    describe('Product Price logic', () => {
+      it('stores the dollar price value in pennies', () => {
+        expect(costume.price).to.equal(6969);
+      });
+
+      it('converts the price back into dollars', () => {
+        expect(costume.priceInDollars).to.equal('69.69');
+      });
+
+      it('has two places after the decimal including zero endings', () => {
+        expect(costume2.priceInDollars).to.equal('50.00')
+      });
     });
   });
 });
