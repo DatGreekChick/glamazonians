ffa37fdd0d4386b850f5411960dcf59b9555f625
Cecilia Chang <ceciliafchang@gmail.com>
2017-11-02 13:14:15 -0400

    finished model specs and model testing

--- server/api/products.js
+++ server/api/products.js
@@ -1,7 +1,6 @@
-const router = require('express').Router()
-const {Product, Order} = require('../db/models')
-module.exports = router
-
+const router = require('express').Router();
+const { Product, Order } = require('../db/models');
+module.exports = router;
 
 // GET /api/products
 router.get('/', (req, res, next) => {
@@ -13,45 +12,44 @@ router.get('/', (req, res, next) => {
 // GET /api/products/:productId
 router.get('/:productId', (req, res, next) => {
   Product.findById(req.params.productId)
-  .then(product => res.json(product))
-  .catch(next);
+    .then(product => res.json(product))
+    .catch(next);
 });
 
 // POST /api/products
 router.post('/', (req, res, next) => {
   Product.create(req.body)
-  .then(product => res.json(product))
-  .catch(next);
+    .then(product => res.status(201).json(product))
+    .catch(next);
 });
 
 // PUT /api/products
 router.put('/:productId', (req, res, next) => {
-  Product.findById(req.params.productId)
-  .then(product => product.update(req.body))
-  .then(res.end())
-  .catch(next);
+  Product.findById(+req.params.productId)
+    .then(product => product.update(req.body))
+    .then(res.sendStatus(202))
+    .catch(next);
 });
 
 // DELETE /api/products/:productId
 router.delete('/:productId', (req, res, next) => {
   Product.destroy({
-    where:
-    { id: req.params.productId }
+    where: { id: +req.params.productId }
   })
-  .then(() => res.status(204).end())
-  .catch(next);
+    .then(() => res.status(204).end())
+    .catch(next);
 });
 
 // GET /api/products
 router.get('/:tag', (req, res, next) => {
   Product.findSimilarByTag(req.params.tag)
-  .then(products => res.json(products))
-  .catch(next);
+    .then(products => res.json(products))
+    .catch(next);
 });
 
 // TODO: write Order model method
 router.get('/popular', (req, res, next) => {
   Order.getPopularProducts({})
-  .then(products => res.json(products))
-  .catch(next);
+    .then(products => res.json(products))
+    .catch(next);
 });
--- server/db/models/product.js
+++ server/db/models/product.js
@@ -1,63 +1,61 @@
-const {
-  STRING,
-  DECIMAL,
-  INTEGER,
-  TEXT,
-  ARRAY
-} = require('sequelize');
+const { STRING, INTEGER, TEXT, ARRAY } = require('sequelize');
 const db = require('../db');
 
-const Product = db.define('product', {
-  name: {
-    type: STRING,
-    unique: true,
-    allowNull: false
-  },
-  image: {
-    type: STRING,
-    defaultValue: 'http://placecage.com/400/400'
-  },
-  price: {
-    type: INTEGER,
-    set: function(val) {
-      let dollars;
-      if (typeof val === 'string') {
-        dollars = parseInt(val);
-      } else {
-        dollars = val;
+const Product = db.define(
+  'product',
+  {
+    name: {
+      type: STRING,
+      unique: true,
+      allowNull: false
+    },
+    image: {
+      type: STRING,
+      defaultValue: 'http://placecage.com/400/400'
+    },
+    price: {
+      type: INTEGER,
+      set: function(val) {
+        let dollars;
+        if (typeof val === 'string') {
+          dollars = parseInt(val);
+        } else {
+          dollars = val;
+        }
+        this.setDataValue('price', dollars * 100);
       }
-      this.setDataValue('price', dollars * 100);
+    },
+    quantityAvailable: INTEGER,
+    description: TEXT,
+    tags: {
+      type: ARRAY(STRING),
+      defaultValue: [],
+      set: function(tags) {
+        tags = tags || [];
+
+        if (typeof tags === 'string') {
+          tags = tags.split(',').map(function(str) {
+            return str.trim();
+          });
+        }
+        this.setDataValue('tags', tags);
+      }
     }
   },
-  quantityAvailable: INTEGER,
-  description: TEXT,
-  tags: {
-    type: ARRAY(STRING),
-    defaultValue: [],
-    set: function(tags) {
-      tags = tags || [];
+  {
+    getterMethods: {
+      priceInDollars: function() {
+        let pennies = this.getDataValue('price');
 
-      if (typeof tags === 'string') {
-        tags = tags.split(',').map(function(str) {
-          return str.trim();
-        });
+        return parseFloat(pennies / 100).toFixed(2);
       }
-      this.setDataValue('tags', tags);
     }
   }
-}, {
-  getterMethods: {
-    priceInDollars: function() {
-      let pennies = this.getDataValue('price');
-
-      return parseFloat(pennies / 100).toFixed(2);
-    }
-  },
-});
+);
 
 // instanceMethods
 Product.prototype.removeTag = function(tag) {
-  tag = tag.toString()
+  tag = tag.toString();
   let arrCopy = this.getDataValue('tags');
   let index = arrCopy.indexOf(tag.toLowerCase());
 
@@ -67,7 +65,7 @@ Product.prototype.removeTag = function(tag) {
   this.setDataValue('tags', arrCopy);
 };
 
-Product.prototype.addTag =  function(tag) {
+Product.prototype.addTag = function(tag) {
   tag = tag.toString();
   let arrCopy = this.getDataValue('tags');
   arrCopy.push(tag.toLowerCase());
