44660b6ee6f897c0cb8be05cbdf8c0ae1e2c7c5d
KeriPMiller <miller.keripatricia@gmail.com>
2017-11-05 17:30:09 -0500

    single product start

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router';
+import { addItem, increaseItem } from '../store';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
 
-const SingleProduct = () => {
-  return (
-    <div>
-      SingleProduct
-    </div>
-  );
+
+class SingleProduct extends Component {
+
+  componentWillMount(){
+    this.productId = this.props.match.params.productId
+  }
+
+  render() {
+    {console.log(this.productId)}
+    return (
+      <div>
+        SingleProduct
+      </div>
+    );
+  }
 };
 
-export default SingleProduct;
+const mapStateToProps = (state) => {
+  return {
+    products: state.products,
+    cart: state.cart,
+    user: state.user
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  onAdd (product) {
+      dispatch(addItem(product))
+    },
+  onIncrease (product) {
+        dispatch(increaseItem(product))
+      }
+    }
+);
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SingleProduct));
