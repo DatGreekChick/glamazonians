dd3a1c7f855a50358af77e2bc515293a60d342d3
srackley <shelbyrackleyart@gmail.com>
2017-11-06 17:31:57 -0500

    fix single product

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,108 +1,147 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-import { fetchSingleProduct, addItem, increaseItem, decreaseItem } from '../store';
-import { NotificationContainer, NotificationManager } from 'react-notifications';
+import {
+  fetchSingleProduct,
+  addItem,
+  increaseItem,
+  decreaseItem
+} from '../store';
+import {
+  NotificationContainer,
+  NotificationManager
+} from 'react-notifications';
 
 class SingleProduct extends Component {
-  constructor() {
-    super();
-  }
-
   componentWillMount() {
     const productId = this.props.match.params.productId;
     this.props.fetchSelectedProduct(productId);
+  }
 
+  addHandler(product) {
+    NotificationManager.success(
+      `You added ${product.name} to your cart`,
+      'Added Item'
+    );
+    if (this.props.cart.indexOf(product) > -1) {
+      this.props.onIncrease(product);
+    } else {
+      this.props.onAdd(product);
+    }
+  }
+
+  decreaseHandler(product) {
+    NotificationManager.warning(
+      `You removed one ${product.name} from your cart`,
+      'Removed Item'
+    );
+    this.props.onDecrease(product);
+  }
+
+  increaseHandler(product) {
+    NotificationManager.success(
+      `You added one ${product.name} to your cart`,
+      'Added Item'
+    );
+    this.props.onIncrease(product);
   }
 
   render() {
     const product = this.props.product;
-    console.log(this.props.cart)
+    console.log(this.props.cart);
     return (
-      <div className='products single-product'>
-        <h1 className='title'>{product.name}</h1>
-        <img src={product.image}/>
+      <div className="products oneProduct">
+        <h1 className="title">{product.name}</h1>
+        <img src={product.image} />
 
         <div className="product-info">
-          <h3> &#36; {product.priceInDollars} </h3>
-          <span>Quantity: {product.quantityInCart}</span>
-          <div className='button-container'>
-            <button
-              className="btn btn-info" onClick={() => {
-            NotificationManager.success(`You added ${product.name} to your cart`, 'Added Item');
-              if (this.props.cart.indexOf(product) > -1){
-                this.props.onIncrease(product)
-              } else {
-                this.props.onAdd(product);
-              }
-              }}>ADD</button>
-            <button onClick={() => {
-              NotificationManager.success(`You added one ${product.name} to your cart`, 'Added Item');
-              this.props.onIncrease(product);
+          <h2> &#36; {product.priceInDollars} </h2>
+          <h4>{product.description}</h4>
+          <span>
+            <h3>Quantity: {product.quantityInCart}</h3>
+          </span>
+          <button
+            onClick={() => {
+              this.increaseHandler(product);
             }}
             disabled={product.quantityInCart < 1}
-            >
-              &#x21e7;
-            </button>
+          >
+            &#x21e7;
+          </button>
+          <button
+            onClick={() => {
+              this.decreaseHandler(product);
+            }}
+            disabled={product.quantityInCart <= 1}
+          >
+            &#x21e9;
+          </button>
+          <div>
             <button
+              className="addButton"
               onClick={() => {
-                NotificationManager.warning(
-                  `You removed one ${product.name} from your cart`,
-                  'Removed Item'
-                );
-                this.props.onDecrease(product);
+                this.addHandler(product);
               }}
-              disabled={product.quantityInCart <= 1}
             >
-              &#x21e9;
+              Add To Cart
             </button>
           </div>
-          <div className='review-container'>
+          <div className="review-container">
             <h2>Reviews</h2>
-            <hr/>
-            {product.reviews.map(review => {
-             return(  <div key={review.id}>
-                <h3>{review.title}</h3>
-                <h4>user: {review.userId}</h4>
-                <span> Rating: {reviewStars(review.rating)}</span>
-                <p>{review.description}</p>
-              </div>)
-            })}
+            <hr />
+            {product.reviews.length &&
+              product.reviews.map(review => {
+                return (
+                  <div key={review.id}>
+                    <h3>{review.title}</h3>
+                    <h4>user: {review.userId}</h4>
+                    <span>Rating: {reviewStars(review.rating)}</span>
+                    <p>{review.description}</p>
+                  </div>
+                );
+              })}
           </div>
         </div>
         <NotificationContainer />
       </div>
     );
   }
-};
+}
 
 function reviewStars(rating) {
-  let starRating = ['☆','☆','☆','☆','☆']
-  for(let i = 0; i < rating; i++){
+  let starRating = ['☆', '☆', '☆', '☆', '☆'];
+  for (let i = 0; i < rating; i++) {
     starRating.pop();
-    starRating.unshift('★')
+    starRating.unshift('★');
   }
-  return (starRating.join(' '));
+  return starRating.join(' ');
 }
 
-const mapStateToProps = (state) => {
-  return {products: state.products, cart: state.cart, user: state.user, product: state.product}
-}
+const mapStateToProps = state => {
+  return {
+    products: state.products,
+    cart: state.cart,
+    user: state.user,
+    product: state.product
+  };
+};
 
-const mapDispatchToProps = (dispatch) => ({
-  fetchSelectedProduct: (product) => {
+const mapDispatchToProps = dispatch => ({
+  fetchSelectedProduct: product => {
     dispatch(fetchSingleProduct(product));
   },
-  onAdd: (product) => {
-    dispatch(addItem(product))
+  onAdd: product => {
+    dispatch(addItem(product));
   },
-  onIncrease: (product) => {
+  onIncrease: product => {
     dispatch(increaseItem(product));
   },
-  onDecrease: (product) => {
+  onDecrease: product => {
     dispatch(decreaseItem(product));
   }
 });
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SingleProduct));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(SingleProduct)
+);
