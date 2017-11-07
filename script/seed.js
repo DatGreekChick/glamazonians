ef78aac19042024fdcb309a7c07914f67dad5480
Eleni A <eleni.arvanitis@me.com>
2017-11-07 16:50:26 -0500

    still adding to seed file

--- script/seed.js
+++ script/seed.js
@@ -96,57 +96,59 @@ async function seed() {
       rating: 4,
       description: 'This role was super great for Nic Cage. It really added depth to his character portfolio.',
       productId: 3,
-      userId: 3
+      userId: 4
     }),
     Review.create({
       title: 'This guy is the worst',
       rating: 1,
       description: 'Does this guy even act? Like DOES. 👏 HE. 👏 EVEN!?',
-      productId: 3,
-      userId: 3
+      productId: 4,
+      userId: 2
     }),
     Review.create({
       title: 'What a stud',
       rating: 5,
-      description: 'I fan girl at the thought of Nic Cage.',
-      productId: 3,
-      userId: 3
+      description: 'I fan-girl at the thought of Nic Cage.',
+      productId: 6,
+      userId: 4
+    }),
+    Review.create({
+      rating: 5,
+      description: 'All these trolls need to go. Sure, Nic Cage might no longer be in the best films of our time,' +
+      ' but he is a valued actor in the community. People should be aware of his talent, and just be nice!',
+      productId: 7,
+      userId: 4
     }),
     Review.create({
       rating: 1,
-      description: 'NEVER AGAIN! HATED THIS!',
-      productId: 3,
-      userId: 3
+      description: 'No.',
+      productId: 8,
+      userId: 5
+    }),
+    Review.create({
+      rating: 4,
+      description: 'SO GOOD!',
+      productId: 7,
+      userId: 6
+    }),
+    Review.create({
+      rating: 5,
+      description: 'I\'ve never written a review before, but I feel like it was necessary this time. Nic Cage should' +
+      ' teach everyone how it\'s done. Sure, it wasn\'t the most amazing role, but he was great nonetheless.',
+      productId: 9,
+      userId: 7
+    }),
+    Review.create({
+      rating: 3,
+      description: 'I wish that he was more prevalent in the film. With a cigar to boot! *Sigh*',
+      productId: 6,
+      userId: 8
     }),
     Review.create({
       rating: 1,
-      description: 'NEVER AGAIN! HATED THIS!',
+      description: '😫 Who wrote this role for the venerable Nic Cage?!?! Stop this madness in Hollywood!',
       productId: 3,
-      userId: 3
-    }),
-    Review.create({
-      rating: 1,
-      description: 'NEVER AGAIN! HATED THIS!',
-      productId: 3,
-      userId: 3
-    }),
-    Review.create({
-      rating: 1,
-      description: 'NEVER AGAIN! HATED THIS!',
-      productId: 3,
-      userId: 3
-    }),
-    Review.create({
-      rating: 1,
-      description: 'NEVER AGAIN! HATED THIS!',
-      productId: 3,
-      userId: 3
-    }),
-    Review.create({
-      rating: 1,
-      description: 'NEVER AGAIN! HATED THIS!',
-      productId: 3,
-      userId: 3
+      userId: 9
     })
   ]);
 
@@ -177,7 +179,7 @@ async function seed() {
   const addresses = await Promise.all([
     Address.create({
       line1: '123 Road Way',
-      line2: '',
+      line2: 'Apt #5',
       city: 'New York',
       state: 'NY',
       zip: '10003',
@@ -197,16 +199,64 @@ async function seed() {
       city: 'New York',
       state: 'NY',
       zip: '10003',
+      userId: 4
+    }),
+    Address.create({
+      line1: '227 Sullivan Street',
+      line2: 'Apt. D5',
+      city: 'New York',
+      state: 'NY',
+      zip: '10003',
+      userId: 5
+    }),
+    Address.create({
+      line1: '28 W 26th Street',
+      line2: 'Apt. 1C',
+      city: 'New York',
+      state: 'NY',
+      zip: '10019',
+      userId: 6
+    }),
+    Address.create({
+      line1: '40 Hanover Street',
+      line2: 'Floor 25',
+      city: 'New York',
+      state: 'NY',
+      zip: '10001',
+      userId: 7
+    }),
+    Address.create({
+      line1: '1 Road Way',
+      line2: 'Apt. 25',
+      city: 'New York',
+      state: 'NY',
+      zip: '10003',
+      userId: 8
+    }),
+    Address.create({
+      line1: '1 Six Flags Boulevard',
+      line2: '',
+      city: 'Jackson',
+      state: 'NJ',
+      zip: '08527',
+      userId: 9
+    }),
+    Address.create({
+      line1: '1 World Way',
+      line2: 'Walt Disney World Resort',
+      city: 'Orlando',
+      state: 'FL',
+      zip: '32830',
       userId: 3
     })
   ]);
 
   console.log(`seeded ${addresses.length} addresses`);
 
-  // using default images here for the time being
   const products = await Promise.all([
     Product.create({
       name: 'Archer Jacket',
+      image: 'http://placecage.com/200/200',
       price: 25.5,
       quantityAvailable: 10,
       description: 'Totally not Katniss from The Hunger Games',
@@ -215,6 +265,7 @@ async function seed() {
     }),
     Product.create({
       name: 'Courageous Forest Princess',
+      image: 'http://placecage.com/400/400',
       price: 25.99,
       quantityAvailable: 25,
       description: 'Totally not Merida from Brave',
@@ -223,6 +274,61 @@ async function seed() {
     }),
     Product.create({
       name: 'K Billy Skin Suit',
+      image: 'http://placecage.com/500/500',
+      price: 35.5,
+      quantityAvailable: 30,
+      description: 'Totally not Uma Thurman in Kill Bill.',
+      tags: ['uma', 'kill', 'bill', 'yellow'],
+      orderId: 3
+    }),
+    Product.create({
+      name: 'K Billy Skin Suit',
+      image: 'http://placecage.com/700/500',
+      price: 35.5,
+      quantityAvailable: 30,
+      description: 'Totally not Uma Thurman in Kill Bill.',
+      tags: ['uma', 'kill', 'bill', 'yellow'],
+      orderId: 1
+    }),
+    Product.create({
+      name: 'K Billy Skin Suit',
+      image: 'http://placecage.com/100/200',
+      price: 35.5,
+      quantityAvailable: 30,
+      description: 'Totally not Uma Thurman in Kill Bill.',
+      tags: ['uma', 'kill', 'bill', 'yellow'],
+      orderId: 2
+    }),
+    Product.create({
+      name: 'K Billy Skin Suit',
+      image: 'http://placecage.com/200/300',
+      price: 35.5,
+      quantityAvailable: 30,
+      description: 'Totally not Uma Thurman in Kill Bill.',
+      tags: ['uma', 'kill', 'bill', 'yellow'],
+      orderId: 3
+    }),
+    Product.create({
+      name: 'K Billy Skin Suit',
+      image: 'http://placecage.com/800/700',
+      price: 35.5,
+      quantityAvailable: 30,
+      description: 'Totally not Uma Thurman in Kill Bill.',
+      tags: ['uma', 'kill', 'bill', 'yellow'],
+      orderId: 1
+    }),
+    Product.create({
+      name: 'K Billy Skin Suit',
+      image: 'http://placecage.com/800/900',
+      price: 35.5,
+      quantityAvailable: 30,
+      description: 'Totally not Uma Thurman in Kill Bill.',
+      tags: ['uma', 'kill', 'bill', 'yellow'],
+      orderId: 2
+    }),
+    Product.create({
+      name: 'K Billy Skin Suit',
+      image: 'http://placecage.com/700/300',
       price: 35.5,
       quantityAvailable: 30,
       description: 'Totally not Uma Thurman in Kill Bill.',
